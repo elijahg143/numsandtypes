f1b6077e0c6c7a9586927cf9ca49f7b7392256ad
elijahg143 <144160987+elijahg143@users.noreply.github.com>
2024-02-01 15:31:17 -0500

    Add files via upload

---
+++ 3.1to3.6.py
@@ -0,0 +1,23 @@
+#Elijah Gonzalez
+#Purpose: Excercises
+#Variable Dictionary:
+
+    
+#3.1
+seconds = 60
+minutes = 60
+
+#3.2
+seconds_per_hour = (seconds * minutes)
+print(seconds_per_hour)
+
+#3.3 and 3.4
+seconds_per_day = (seconds_per_hour * 24)
+print(seconds_per_day)
+
+#3.5
+print(seconds_per_day / seconds_per_hour)
+
+#3.6
+print(seconds_per_day // seconds_per_hour)
+
